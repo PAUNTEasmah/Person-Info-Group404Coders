51df217d81dad4fca192eeca5d0fb91b8879f248
XantiQuinal2026 <xmlquinal@cmc.pshs.edu.ph>
2026-03-18 08:12:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Hello " + name)
 age = input("12? ")
 addr = input("Iligan City? ")
 print(name + " lives in " + addr)
+age = int(input("Enter your age: "))
+year = 2026 - age
+print("Born in: " + str(year))
